b7e97dc7b58e7f6d06aa028ef63a8a0c78562664
Jonathan Ryan <jonny788@live.co.uk>
2021-01-03 20:14:18 +0000

    added all primary runes

--- runes.py
+++ runes.py
@@ -20,5 +20,73 @@ class Primary:
         "cast stand" : (500,760)
     }
     domination = {
-        
+        "electrocute" : (320,420),
+        "predator" : (380,420),
+        "dark harvest" : (440,420),
+        "hail of blades" : (500,420),
+
+        "cheap shot" : (320,540),
+        "taste of blood" : (400,540),
+        "sudden impact"	 : (500,540),
+
+        "zombie ward" :	(320,650),
+        "ghost poro" :	(400,650),
+        "eyeball collection" : (500,650),
+
+        "ravenous hunter" :	(320,760),
+        "ingenious hunter" : (380,760),
+        "relentless hunter" :	(440,760),
+        "ultimate hunter" :	(500,760)
     }
+    sourcery = {
+        "summon aery" : (320,420),
+        "arcane comet" : (400,420),
+        "phase rush" : (500,420),
+
+        "nullifiying orb" : (320,540),
+        "manaflow band" :	(400,540),
+        "nimbus cloak" : (500,540),
+
+        "transcendence"	: (320,650),
+        "celerity" : (400,650),
+        "absolute focus" : (500,650),
+
+        "scorch" : (320,760),
+        "waterwalking" : (400,760),
+        "gathering storm" : (500,760)
+    }
+    resolve = {
+        "grasp of the undying" : (320,420),
+        "aftershock" :	(400,420),
+        "guardian" : (500,420),
+
+        "demolish" : (320,540),
+        "font of life" : (400,540),
+        "shield bash" : (500,540),
+
+        "conditioning" : (320,650),
+        "second wind" : (400,650),
+        "bone plating" : (500,650),
+
+        "overgrowth" :	(320,760),
+        "revitalize" : (400,760),
+        "unflinching" : (500,760)
+    }
+    inspiration = {
+        "glacial augment" :	(320,420),
+        "unsealed spellbook" : (400,420),
+        "prototype: omnistone" : (500,420),
+
+        "hextech flashtraption" : (320,540),
+        "magical footwear" : (400,540),
+        "perfect timing" : (500,540),
+
+        "future market"	: (320,	650),
+        "minion dematerializer": (400,650),
+        "biscuit delivery": (500,650),
+
+        "cosmic insight" : (320,760),
+        "approach velocity"	: (400,760),
+        "time warp tonic" : (500,760),
+    }
+
