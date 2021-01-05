42af83938f6346741751ad6032e76fae82cf609f
Jonathan Ryan <jonny788@live.co.uk>
2021-01-05 00:40:11 +0000

    bouns scrap and conversion added need magic resist

--- scrap.py
+++ scrap.py
@@ -8,10 +8,10 @@ soup = BeautifulSoup(req.text, "html.parser")
 
 runeBlock = soup.find("div",{"class": "view-guide__build__runes"})
 
-primaryRuneBlock = soup.find("div", {"class": "new-runes__primary"})
 
-#print(primaryRuneBlock)
 
+
+primaryRuneBlock = runeBlock.find("div", {"class": "new-runes__primary"})
 primaryPath = primaryRuneBlock.get("path")
 primaryRunesHtml = primaryRuneBlock.find_all("span")
 primaryRunes = []
@@ -20,5 +20,50 @@ for i in range(len(primaryRunesHtml)):
 
 primaryRunes.pop(0)
 
-print(primaryPath)
-print(primaryRunes)
+#print(primaryPath)
+#print(primaryRunes)
+
+print("")
+
+secondaryRuneBlock = runeBlock.find("div", {"class": "new-runes__secondary"})
+secondaryPath = secondaryRuneBlock.get("path")
+secondaryRunesHtml = secondaryRuneBlock.find_all("span")
+secondaryRunes = []
+for i in range(len(secondaryRunesHtml)):
+    secondaryRunes.append(secondaryRunesHtml[i].text)
+secondaryRunes.pop(0)
+
+
+#print(secondaryPath)
+#print(secondaryRunes)
+
+
+bonusDict = {
+    "diamond" : "adaptive force",
+    "axe" : "attack speed",
+    "time" : "ability haste",
+    "shield" : "armor",
+    "heart" : "health",
+    #magic resist
+}
+
+bonusBlock = runeBlock.find("div", {"class": "new-runes__shards"})
+bonusHtml = bonusBlock.find_all("span")
+bonus = []
+#bonus span tags differ from acutal names so must be converted
+for i in range(len(bonusHtml)):
+    bonus.append( bonusDict[bonusHtml[i].get("shard-type")] )
+print(bonus)
+
+
+
+#construct the input rune page for main program
+
+
+
+#runepage = []
+#runepage.append(primaryPath)
+#runepage.append(secondaryPath)
+#runepage.extend(primaryRunes)
+
+#print(runepage)
